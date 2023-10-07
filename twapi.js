edf5bf1b64148cc67b65f75ff3fc33f63a81cb57
Hector2523 <67517312+Hector2523@users.noreply.github.com>
2023-10-07 16:29:37 -0300

    dgr

---
+++ twapi.js
@@ -0,0 +1,40 @@
+var channelName = undefined;
+var chatSelect = document.getElementById("chat");
+function embedTwitchChannel() {
+  channelName = document.getElementById("escolhaCanal").value;
+  document.getElementById("twitch-embed").innerHTML = "";
+  
+  new Twitch.Player("twitch-embed", {
+      channel: channelName
+  });
+
+  document.getElementById("twitch-embed").style.animation = "box 2s forwards";
+}
+
+function abrirChat() {
+
+  const iframe = document.getElementById("twitch-chat-embed");
+  if ( channelName == undefined ){
+    alert("Selecione o canal primeiro");
+    return false;
+  }
+  
+  console.log("abrirChat(): iFrame  display=["+ getComputedStyle(iframe).display +"]");
+  switch (chatSelect.value) {
+      case "0": 
+      document.getElementById("twitch-embed").style.animation = "box 1.5s forwards";
+      break;
+      case "1":
+          iframe.style.display = "block";
+          iframe.setAttribute("src", "https://www.twitch.tv/embed/"+channelName+"/chat?parent=localhost");
+          document.getElementById("twitch-embed").style.animation = "boxNone 1.5s forwards";
+          break;
+      case "2":
+          iframe.style.display = "none";
+          document.getElementById("twitch-embed").style.animation = "box 1.5s forwards";
+          break;
+      default:
+          alert("Opção inválida");
+  }
+  console.log("abrirChat(): iFrame NEW display=["+ getComputedStyle(iframe).display +"]");
+}
